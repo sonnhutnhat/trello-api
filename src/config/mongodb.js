3a14a6c34f752446b69ad5866cd45fdd687a3ecb
sonnhutnhat <nguyenkienson3010@gmail.com>
2025-10-01 15:13:49 +0700

    Connect MongoDB Env Variables

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,37 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from '~/config/environment'
+
+// Khởi tạo 1 đối tượng trelloDatabaseInstance ban đầu là null(vì chúng ta chưa connect)
+let trelloDatabaseInstance = null
+
+// Khởi tạo 1 đối tượng mongoClientInstance để connect tới mongodb
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+// Connect toi DB
+export const CONNECT_BD = async () => {
+  // Gọi kết nối tới MongoDb Atlas với URL đã khai báo trong thân của mongoClientInstance
+  await mongoClientInstance.connect()
+
+  // Kết nối thành công thì lấy ra Database theo tên và gán ngược lại vào biến trelloDatabaseInstance ở trên
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+}
+
+// Đóng kết nối tới Database khi cần
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must Connect to Database First')
+  return trelloDatabaseInstance
+}

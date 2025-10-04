72386f8008f69ac8d31743c7ae34659b8dad27a1
sonnhutnhat <nguyenkienson3010@gmail.com>
2025-10-05 01:15:06 +0700

    Controller Handling

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -8,20 +8,21 @@ import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
-    title: Joi.string().required().min(3).max(50).trim().strict(),
-    description: Joi.string().required().min(3).max(256).trim().strict()
+    title: Joi.string().required().min(3).max(50).trim().strict().messages({
+
+    }),
+    description: Joi.string().required().min(3).max(255).trim().strict()
   })
   try {
     console.log(req.body)
-    // Chỉ định abortEarly: false để trường hợp có nhiều lỗi validation thì trả về tất cả lỗi 
+    // Chỉ định abortEarly: false để trường hợp có nhiều lỗi validation thì trả về tất cả lỗi
     await correctCondition.validateAsync(req.body, { abortEarly: false })
-
-    // next()
-    res.status(StatusCodes.CREATED).json({ message: 'POST from Validation: APIs create new board' })
+    // Validate dữ liệu xong xuôi hợp lệ thì cho request đi tiếp sang Controller
+    next()
   } catch (error) {
     console.log(error)
     res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error.message)
+      errors: new Error(error).message
     })
   }
 }

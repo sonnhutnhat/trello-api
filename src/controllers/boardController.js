481e1de8cef38ed3cf4557804a32d4aab585cb80
sonnhutnhat <nguyenkienson3010@gmail.com>
2025-10-13 22:25:49 +0700

    Error Handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -4,15 +4,23 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   try {
-    console.log(req.body)
+    // console.log('req.body: ', req.body)
+    // console.log('req.query: ', req.query)
+    // console.log('req.params: ', req.params)
+    // console.log('req.files: ', req.files)
+    // console.log('req.cookies: ', req.cookies)
+    // console.log('req.jwtDecoded: ', req.jwtDecoded)
+
+    //Điều hướng tầng service
+
+    // Có kết quả thì trả về phía client
     res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: APIs create new board' })
   } catch (error) {
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: error.message
-    })
+    next(error)
   }
 }
 

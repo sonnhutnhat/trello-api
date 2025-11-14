35622a05a128f212ad4e090361fd42e43768127c
sonnhutnhat <nguyenkienson3010@gmail.com>
2025-11-14 11:51:38 +0700

    70 DND Columns

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -35,9 +35,9 @@ const createNew = async (reqBody) => {
   } catch (error) { throw error }
 }
 
-const getDetails = async (boardID) => {
+const getDetails = async (boardId) => {
   try {
-    const board = await boardModel.getDetails(boardID)
+    const board = await boardModel.getDetails(boardId)
     if (!board) {
       throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found!')
     }
@@ -56,7 +56,21 @@ const getDetails = async (boardID) => {
   } catch (error) { throw error }
 }
 
+const update = async (boardId, reqBody) => {
+  try {
+    const updateData = {
+      ...reqBody,
+      updatedAt: Date.now()
+    }
+    const updatedBoard = await boardModel.update(boardId, updateData)
+
+
+    return updatedBoard
+  } catch (error) { throw error }
+}
+
 export const boardService = {
   createNew,
-  getDetails
+  getDetails,
+  update
 }
